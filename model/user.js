4887a921b6444a4b59df82fcb33a129fe49bb65a
oghene-ella <oghenekaro57@gmail.com>
2024-02-27 12:17:36 -0600

    updated the nodemailer utility and the user model

--- model/user.js
+++ model/user.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username:{type:String},
-    email:{type:String},
-    dob:{type:Date}
-})
+    username: { type: String },
+    email: { type: String },
+    dob: {
+        type: Date,
+    }
+});
 
-module.exports = mongoose.model("User", userSchema)
+const UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -2,18 +2,18 @@ const nodemailer = require("nodemailer")
 require("dotenv").config()
 
 const sendEmail = async(option)=>{
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth:{
-        user:process.env.USER_EMAIL,
-        pass:process.env.USER_PASS
-    }
-})
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth:{
+            user:process.env.USER_EMAIL,
+            pass:process.env.USER_PASS
+        }
+    })
     await transporter.sendMail({
-      from:process.env.USER_EMAIL,
-      to:option.email,
-      subject:option.subject,
-      html:option.html  
+        from:process.env.USER_EMAIL,
+        to:option.email,
+        subject:option.subject,
+        html:option.html  
     })
 }
 
